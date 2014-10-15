0b51d2ab6777d28ace1d7de146a335f3289ae2cf
Damian Hites <Nizebulous@gmail.com>
2014-10-15 11:14:27 -0700

    New cookbook recipe referencing the etcd cookbook

---
+++ rdio_etcd/metadata.rb
@@ -0,0 +1,9 @@
+name             'rdio_etcd'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rdio_etcd'
+long_description 'Installs/Configures rdio_etcd'
+version          '0.1.0'
+
+depends 'etcd', '~> 1.1.0'
---
+++ rdio_etcd/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: rdio_etcd
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'etcd-cookbook::default'
+include_recipe 'etcd-cookbook::cluster'
