e8a68d73a47be3444d502d96f3719dbadd2eaaa6
Damian Hites <Nizebulous@gmail.com>
2014-10-16 15:22:14 -0700

    Removing etcd cookbook
    Can be deployed with docker now. No need for it anymore.

--- rdio_etcd/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'rdio_etcd'
-maintainer       'Damian Hites'
-maintainer_email 'damian.hites@rd.io'
-license          'All rights reserved'
-description      'Installs/Configures rdio_etcd'
-long_description 'Installs/Configures rdio_etcd'
-version          '0.1.0'
-
-depends 'etcd', '~> 2.2.2'
